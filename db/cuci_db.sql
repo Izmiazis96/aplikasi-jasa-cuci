53ca85b66cc89ada3d173ce5732d2c1c661d4c1e
Izmiazis96 <mohamadismiazis@gmail.com>
2022-04-21 13:18:22 +0700

    commit_project_awal

---
+++ db/cuci_db.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 21 Apr 2022 pada 08.15
+-- Versi server: 10.1.38-MariaDB
+-- Versi PHP: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cuci_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `biaya`
+--
+
+CREATE TABLE `biaya` (
+  `id_biaya` int(10) NOT NULL,
+  `jenis` varchar(50) NOT NULL,
+  `biaya` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `biaya`
+--
+
+INSERT INTO `biaya` (`id_biaya`, `jenis`, `biaya`) VALUES
+(1, 'Sedan', 20000),
+(2, 'Motor Kecil', 15000),
+(3, 'Motor Besar', 20000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(10) NOT NULL,
+  `no_nota` varchar(10) NOT NULL,
+  `jenis` varchar(50) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `bayar` int(10) NOT NULL,
+  `kembali` int(10) NOT NULL,
+  `total` int(10) NOT NULL,
+  `tanggal` date NOT NULL,
+  `id_user` tinyint(1) NOT NULL,
+  `biaya_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `no_nota`, `jenis`, `nama`, `bayar`, `kembali`, `total`, `tanggal`, `id_user`, `biaya_id`) VALUES
+(2, 'C001', 'Motor Besar', 'Ando', 50000, 30000, 20000, '2022-04-21', 2, 0),
+(3, 'C002', 'Motor Kecil', 'rizal jihaduddin', 15000, 0, 15000, '2022-04-21', 2, 0),
+(4, 'C003', 'Motor Kecil', 'rizal', 15000, 0, 15000, '2022-04-21', 2, 0),
+(5, 'C004', 'Motor Besar', 'nanda', 25000, 5000, 20000, '2022-04-21', 2, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` tinyint(2) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `nama` varchar(11) NOT NULL,
+  `alamat` varchar(150) NOT NULL,
+  `hp` varchar(20) NOT NULL,
+  `level` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama`, `alamat`, `hp`, `level`) VALUES
+(1, 'rolando', 'e80d4ab77eb18a4ca350157fd487d7e2', 'Rolando', 'Dadap', '08126464', 1),
+(2, 'ismi', '0192023a7bbd73250516f069df18b500', 'ismi azis', 'cengkareng, jakarta barat', '0895334929462', 1),
+(3, 'agnes', '7067821a488137b801cd31ee38fcce05', 'Agnes Perma', 'kalideres, jakarta barat', '089521214343', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `biaya`
+--
+ALTER TABLE `biaya`
+  ADD PRIMARY KEY (`id_biaya`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `biaya`
+--
+ALTER TABLE `biaya`
+  MODIFY `id_biaya` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` tinyint(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
